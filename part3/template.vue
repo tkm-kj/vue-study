1c3e78dcd60f67348f9c8d2a1469eb267ae82505
Takumi Kaji <ivezuki3@gmail.com>
2019-07-14 19:19:21 +0900

    Add part3

---
+++ part3/template.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="login-template">
+        <div>
+            <input type="text" placeholder="ログインID" v-model="userid">
+        </div>
+        <div>
+            <input type="password" placeholder="パスワード" v-model="password">
+        </div>
+        <button @click="login">ログイン</button>
+    </div>
+</template>
